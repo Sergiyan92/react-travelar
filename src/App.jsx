2699530c2a2a497c34b801449f7d953cef395cd8
Sergiyan92 <sergiyan@ukr.net>
2024-09-05 11:52:23 +0300

    create first component

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import IButton from "./component/IButton/IButton";
+import mapImage from "./assets/img/static-map.png";
+import markerIcon from "./assets/img/map-pin.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main className="flex h-screen">
+      <section className="flex-1 flex justify-center items-center px-5 bg-primary">
+        <div className="text-white text-center">
+          <img className="inline mb-6" src={markerIcon} alt="Marker icon" />
+          <h1 className="font-bold text-4xl mb-7">IT traveler</h1>
+          <p className="leading-6 mb-11">
+            Простий і зручний веб додаток, який дозволить тобі відмічати твої
+            улюблені місця, а також ті, в яких би ти дуже хотів побувати. Тож не
+            зволікай і спробуй сам.
+          </p>
+          <IButton></IButton>
+        </div>
+      </section>
+      <section className="flex-1">
+        <img className="h-full w-full object-cover" src={mapImage} alt="Map" />
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
---
+++ src/component/IButton/IButton.jsx
@@ -0,0 +1,9 @@
+const IButton = () => {
+  return (
+    <button className="bg-[#FFA279] rounded-xl py-3 px-10 text-white font-bold -tracking-wider">
+      Почати роботу
+    </button>
+  );
+};
+
+export default IButton;
